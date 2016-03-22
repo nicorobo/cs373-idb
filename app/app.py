340c3d29e51242340b07bd4c6d4ee8a937e64ee7
nickroberts404 <nickroberts404@gmail.com>
2016-03-22 18:51:58 -0500

    Added placeholder endpoints to flask app

--- app/app.py
+++ app/app.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 
 manager = Manager(app)
 
+# Serves the initial website
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Serves JSON for characters
+@app.route('/characters')
+def characters():
+    return 0
+
+# Serves JSON for comics
+@app.route('/comics')
+def comics():
+    return 0
+
+# Serves JSON for creators
+@app.route('/creators')
+def creators():
+    return 0
 
 if __name__ == '__main__':
     manager.run()
