686e8e1049a14f7a6c3824b37fc8efe348b86dd8
Danny Chen <dannychen579@utexas.edu>
2016-03-24 16:01:42 -0500

    scraper.sh now prints the url it generates

--- scrape.sh
+++ scrape.sh
@@ -10,4 +10,6 @@ priv_key=93a16fea6455a62efd7af5b70b33fe853ee0c9a8
 pub_key=f28cee0df52d38e16d477eea4fe0ac6d
 params=$2
 
-curl 'gateway.marvel.com'"$endpoint"'?apikey='"$pub_key"'&ts=1&hash='$(echo -n 1$priv_key$pub_key | md5sum | grep -o "[0-9a-z]*")'&'"$params"
+url='gateway.marvel.com'"$endpoint"'?apikey='"$pub_key"'&ts=1&hash='$(echo -n 1$priv_key$pub_key | md5sum | grep -o "[0-9a-z]*")'&'"$params"
+echo "GET $url" 1>&2
+curl "$url"
