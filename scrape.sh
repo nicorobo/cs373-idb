656a7a45ef4d991038f139b7cbc3e9d9965d43ed
Danny Chen <dannychen579@utexas.edu>
2016-03-24 15:36:43 -0500

    Update script to handle additional parameters

--- scrape.sh
+++ scrape.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -lt "1" ] ; then
-    echo "usage: $0 <endpoint>"
+if [ $# -lt "2" ] ; then
+    echo "usage: $0 <endpoint> <params>"
     exit -1
 fi
 
 endpoint=$1
 priv_key=93a16fea6455a62efd7af5b70b33fe853ee0c9a8
 pub_key=f28cee0df52d38e16d477eea4fe0ac6d
+params=$2
 
-curl 'gateway.marvel.com'"$endpoint"'?apikey='"$pub_key"'&ts=1&hash='$(echo -n 1$priv_key$pub_key | md5sum | grep -o "[0-9a-z]*")
+curl 'gateway.marvel.com'"$endpoint"'?apikey='"$pub_key"'&ts=1&hash='$(echo -n 1$priv_key$pub_key | md5sum | grep -o "[0-9a-z]*")'&'"$params"
