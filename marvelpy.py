6093044d923a5d85608e781c47458263f2d5c462
Benjamin Holder <benjamin.holder@utexas.edu>
2016-03-28 02:02:32 -0500

    Created a Python library to access Marvel API.

---
+++ marvelpy.py
@@ -0,0 +1,127 @@
+"""
+marvelpy is a wrapper around the requests library to facilitate queries to the Marvel Comics API. The calls return Requests Response objects.
+"""
+
+import hashlib, random, requests
+
+
+_base_url = 'http://gateway.marvel.com{}'
+_default_key = None
+
+
+def _build_headers(etag=None, **kwargs):
+    headers = {}
+    if etag:
+        headers['If-None-Match'] = etag
+
+    return headers
+
+
+
+def _build_params(params=None, key=None, **kwargs):
+    if not params:
+        params = {}
+    if not key:
+        key = _default_key
+
+    ts = str(random.randrange(10000))
+    m = hashlib.md5()
+    m.update(ts.encode())
+    m.update(key['private'].encode())
+    m.update(key['public'].encode())
+    
+    params['ts'] = ts
+    params['apikey'] = key['public']
+    params['hash'] = m.hexdigest()
+
+    return params
+
+
+
+def read_key(file_name, default=False):
+    """
+    Reads a Marvel API public/private key pair from the file file_name. The first line should be the public key, the second line, the private key.
+    """
+    with open(file_name) as f:
+        key = {}
+        key['public'] = f.readline()[:-1]
+        key['private'] = f.readline()
+
+    if default:
+        set_key(key)
+    
+    return key
+
+
+def set_key(k):
+    global _default_key
+    _default_key = k
+
+
+###
+
+
+
+def get(url, **kwargs):
+    params = _build_params(**kwargs)
+    headers = _build_headers(**kwargs)
+    return requests.get(url, params=params, headers=headers)
+
+
+def character(character_id, **kwargs):
+    url = _base_url.format('/v1/public/characters/{}').format(character_id)
+    return get(url, **kwargs)
+
+
+
+def characters(**kwargs):
+    url = _base_url.format('/v1/public/characters')
+    return get(url, **kwargs)
+
+
+
+def comic(comic_id, **kwargs):
+    url = _base_url.format('/v1/public/comics/{}').format(comic_id)
+    return get(url, **kwargs)
+
+
+
+def comics(**kwargs):
+    url = _base_url.format('/v1/public/comics')
+    return get(url, **kwargs)
+
+
+
+def creator(creator_id, **kwargs):
+    url = _base_url.format('/v1/public/creators/{}').format(creator_id)
+    return get(url, **kwargs)
+
+
+
+def creators(**kwargs):
+    url = _base_url.format('/v1/public/creators')
+    return get(url, **kwargs)
+
+
+
+def event(event_id, **kwargs):
+    url = _base_url.format('/v1/public/events/{}').format(event_id)
+    return get(url, **kwargs)
+
+
+
+def events(**kwargs):
+    url = _base_url.format('/v1/public/events')
+    return get(url, **kwargs)
+
+
+# Stupid name because series is both singular and plural
+def single_series(series_id, **kwargs):
+    url = _base_url.format('/v1/public/series/{}').format(series_id)
+    return get(url, **kwargs)
+
+
+
+def series(**kwargs):
+    url = _base_url.format('/v1/public/series')
+    return get(url, **kwargs)
