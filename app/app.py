46314040eb4247eee1259cc4e619402827bbdd7d
leslierice <leslierice@utexas.edu>
2016-03-29 15:00:53 -0500

    adding create_db function and logging

--- app/app.py
+++ app/app.py
@@ -1,9 +1,35 @@
+import os
+import logging
+
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.debug("Welcome")
+
+
+SQLALCHEMY_DATABASE_URI = \
+    '{engine}://{username}:{password}@{hostname}/{database}'.format(
+        engine='mysql+pymysql',
+        username=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        hostname=os.getenv('MYSQL_HOST'),
+        database=os.getenv('MYSQL_DATABASE'))
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
+db = SQLAlchemy(app)
+
+from models import *
 
 @app.route('/about')
 def about():
@@ -30,6 +56,25 @@ def creators():
 def index(path):
     return render_template('index.html')
 
+@manager.command
+def create_db():
+    logger.debug("create_db")
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.create_all()
+
+@manager.command
+def create_dummy_data():
+    logger.debug("create_test_data")
+    app.config['SQLALCHEMY_ECHO'] = True
+    character = Character(id=1,name='Steve')
+    db.session.add(character)
+    db.session.commit()
+
+@manager.command
+def drop_db():
+    logger.debug("drop_db")
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.drop_all()
 
 if __name__ == '__main__':
     manager.run()
