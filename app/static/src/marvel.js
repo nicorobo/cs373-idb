c1b9dd6bf3c82157fbf4fc2a4df04f51bf7404c8
nickroberts404 <nickroberts404@gmail.com>
2016-04-05 13:52:08 -0500

    Began clientside API wrapper

---
+++ app/static/src/marvel.js
@@ -0,0 +1,50 @@
+// marvel.js
+
+const request = require('request');
+const API = 'http://sweague.me/';
+
+// Retrieve the character with the given ID
+function getCharacter(id, cb){
+	request(API+'character/'+id, (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+//Retrieve characters
+function getCharacters(limit, offset, cb){
+	request(API+'characters', (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+//Retrieve comic with given ID
+function getComic(id, cb){
+	request(API+'comic/'+id, (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+//Retrieve comics
+function getComics(limit, offset, cb){
+	request(API+'comics', (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+//Retrieve creator with given ID
+function getCreator(id, cb){
+	request(API+'creator/'+id, (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+//Retrieve creators
+function getCreators(limit, offset, cb){
+	request(API+'creators/', (error, response, body) => {
+		cb(error, JSON.parse(body));
+	})
+}
+
+module.exports = {
+
+}
