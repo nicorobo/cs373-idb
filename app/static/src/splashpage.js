9ded77903b4886134a2e2936078bb5d2f9c40336
leslierice <leslierice@utexas.edu>
2016-04-05 21:58:58 -0500

    add description to splashpage closes #32

--- app/static/src/splashpage.js
+++ app/static/src/splashpage.js
@@ -10,6 +10,10 @@ class Splash extends React.Component {
 				<NavBar />
 				<div className="container">
 					<img className="splash-image" src="/static/img/Marvel_Comics.png" />
+					<div className="splash-description">
+						<p>Marvel Comics is an American publisher of comic books.</p>
+						<p>Find information here about Marvel characters, creators, and comics.</p>
+					</div>
 					<p className="footer">Data provided by Marvel. @2016 Marvel</p>
 				</div>
 			</div>
