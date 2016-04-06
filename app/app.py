8761e5f45980c50c77f72854d737ccd347021d87
leslierice <leslierice@utexas.edu>
2016-04-06 18:37:48 -0500

    uncomment cors

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# CORS(app)
+CORS(app)
 
 manager = Manager(app)
 db = SQLAlchemy(app)
