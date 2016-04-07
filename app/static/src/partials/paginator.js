f97553dc30111bd3f2904e03b19b9e98e280cd49
nickroberts404 <nickroberts404@gmail.com>
2016-04-06 20:05:39 -0500

    Created empty paginator component

---
+++ app/static/src/partials/paginator.js
@@ -0,0 +1,15 @@
+// paginator.js
+
+var React = require('react');
+var Link = require('react-router').Link;
+
+class Paginator extends React.Component {
+	render() {
+		return (
+			<nav>
+			</nav>
+		)
+	}
+}
+
+module.exports = Paginator;
