dfc1531bedde3cc3b13b691d7fd3186df6adc02e
leslierice <leslierice@utexas.edu>
2016-04-07 15:06:38 -0500

    fixed unit tests bug closes #37

--- app/tests.py
+++ app/tests.py
@@ -8,6 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['TESTING'] = True
 
 db = SQLAlchemy(app)
