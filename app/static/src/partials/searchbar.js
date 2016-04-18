e5308b336e5dd74db9ee28341a25b507e2b0fd13
nickroberts404 <nickroberts404@gmail.com>
2016-04-18 13:38:44 -0500

    Implemented Danny's solution to allow enter for search. Close #60

--- app/static/src/partials/searchbar.js
+++ app/static/src/partials/searchbar.js
@@ -13,13 +13,20 @@ class SearchBar extends React.Component {
         this.setState({searchTerm: event.target.value})
     }
 
+    handleKeyDown(event) {
+        // If return key is pressed, commence search
+        if(event.keyCode === 13) {
+            document.getElementById('searchButton').click();
+        }
+    }
+
 	render() {
 		return (
             <span>
     			<form className="searchForm">
-    				<input type="text" placeholder="Search" onChange={this.handleChange.bind(this)} />
+    				<input type="text" placeholder="Search" onKeyDown={this.handleKeyDown.bind(this)} onChange={this.handleChange.bind(this)} />
     			</form>
-                <Link to={'/search/'+this.state.searchTerm} className="searchButton">Go</Link>
+                <Link id="searchButton" to={'/search/'+this.state.searchTerm} className="searchButton">Go</Link>
             </span>
 		)
 	}
