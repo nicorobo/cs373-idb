fdeb5645622c84e61fc867defb5fbad0899bbbce
nickroberts404 <nickroberts404@gmail.com>
2016-04-26 18:54:52 -0500

    Added basic margin/svg setup

--- app/static/src/legend/main.js
+++ app/static/src/legend/main.js
@@ -1,3 +1,14 @@
 var legend = require('./legend.js');
+var d3 = require('d3');
 
 legend.getChampion(1, (err, data) => console.log(data));
+
+// Setup our visualization
+var margin = {top: 50, right: 50, bottom: 50, left: 50};
+var height = 500 - margin.top - margin.bottom;
+var width = 500 - margin.left - margin.right;
+var svg = d3.select('.legends-page').append('svg')
+	.attr('height', height + margin.top + margin.bottom)
+	.attr('width', width + margin.left + margin.right)
+var g = svg.append('g')
+	.attr('transform', 'translate('+margin.left+','+margin.top+')');
